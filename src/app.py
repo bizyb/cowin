a243ce498a11df734e1f1480eb164de2e2b6e78b
Biz Melesse <bizmelesse@gmail.com>
2021-05-05 22:55:12 -0500

    Customize messaging conditions

--- src/app.py
+++ src/app.py
@@ -1,25 +1,28 @@
 import requests
+import datetime
 import time
-import yagmail
+import smtplib
+import os
 
-SECONDS_TO_SLEEP = 300  # 5 minutes
+SECONDS_TO_SLEEP = 1800  # 30 minutes
 
 state = {
             "state_id": 21,
             "state_name": "Maharashtra"
         }
 district = {
-                "district_id": 395,
-                "district_name": "Mumbai"
+                "district_id": 394,
+                "district_name": "Palghar"
             }
-START_DATE = "01-05-2021"
+now = datetime.datetime.now()
+START_DATE = "{}-{}-{}".format(now.day, now.month, now.year)
 headers = {
     "Accept": "application/json",
     "Accept-Language": "en_US",
     "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
 }
 params = {
-    "district": 395,
+    "district_id": 394,
     "date": START_DATE
 }
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict"
@@ -29,6 +32,7 @@ def request():
     session = requests.Session()
     session.headers.update(headers)
     response = session.get(url, params=params)
+    print(response.json())
     return response.json()
 
 
@@ -41,7 +45,7 @@ def parse_data(json_data):
                 row = {
                     "location": "{}, {}, {}".format(r.get("name"), r.get("block_name"), r.get("district_name")),
                     "date": r.get("date"),
-                    "cost": r.get("fee_type"),
+                    "cost": r.get("fee"),
                     "slots": r.get("slots"),
                     "num_slots": r.get("available_capacity")
                 }
@@ -51,90 +55,43 @@ def parse_data(json_data):
     return data_to_email
 
 
-
-data = {u'sessions': [{u'address': u'',
-                       u'available_capacity': 11,
-                       u'block_name': u'Ward R North Corporation - MH',
-                       u'center_id': 628041,
-                       u'date': u'01-05-2021',
-                       u'district_name': u'Mumbai',
-                       u'fee': u'0',
-                       u'fee_type': u'Free',
-                       u'from': u'09:00:00',
-                       u'lat': 19,
-                       u'long': 72,
-                       u'min_age_limit': 45,
-                       u'name': u'DAHISAR COVID VACC SUBURBUN',
-                       u'pincode': 400068,
-                       u'session_id': u'592ee4c2-a13b-496d-87d6-9cbe00665424',
-                       u'slots': [u'09:00AM-11:00AM',
-                                  u'11:00AM-01:00PM',
-                                  u'01:00PM-03:00PM',
-                                  u'03:00PM-06:00PM'],
-                       u'state_name': u'Maharashtra',
-                       u'to': u'18:00:00',
-                       u'vaccine': u'COVAXIN'},
-                      {u'address': u'',
-                       u'available_capacity': 7,
-                       u'block_name': u'Ward R North Corporation - MH',
-                       u'center_id': 606878,
-                       u'date': u'01-05-2021',
-                       u'district_name': u'Mumbai',
-                       u'fee': u'0',
-                       u'fee_type': u'Free',
-                       u'from': u'09:00:00',
-                       u'lat': 19,
-                       u'long': 72,
-                       u'min_age_limit': 45,
-                       u'name': u'DAHISAR COVID JUMBO -8',
-                       u'pincode': 400068,
-                       u'session_id': u'bf79efbc-e6b4-40e2-91a2-0d7aa1e54c4c',
-                       u'slots': [u'09:00AM-11:00AM',
-                                  u'11:00AM-01:00PM',
-                                  u'01:00PM-03:00PM',
-                                  u'03:00PM-05:00PM'],
-                       u'state_name': u'Maharashtra',
-                       u'to': u'17:00:00',
-                       u'vaccine': u'COVISHIELD'},
-                      {u'address': u'',
-                       u'available_capacity': 4,
-                       u'block_name': u'Ward R North Corporation - MH',
-                       u'center_id': 606906,
-                       u'date': u'01-05-2021',
-                       u'district_name': u'Mumbai',
-                       u'fee': u'0',
-                       u'fee_type': u'Free',
-                       u'from': u'09:00:00',
-                       u'lat': 19,
-                       u'long': 72,
-                       u'min_age_limit': 45,
-                       u'name': u'DAHISAR COVID JUMBO -10',
-                       u'pincode': 400068,
-                       u'session_id': u'87690ffc-36e3-4bf6-b572-f043ca6adb78',
-                       u'slots': [u'09:00AM-11:00AM',
-                                  u'11:00AM-01:00PM',
-                                  u'01:00PM-03:00PM',
-                                  u'03:00PM-05:00PM'],
-                       u'state_name': u'Maharashtra',
-                       u'to': u'17:00:00',
-                       u'vaccine': u'COVISHIELD'}]}
-
-
 def send_email(email_content):
-    for row in email_content:
-        print(row)
+    body = ""
+    if not email_content:
+        body = "No slots found"
+    for ec in email_content:
+        slots = "\n".join(["\t" + slot for slot in ec.get("slots")])
+        text = "Location: {}\nCost: {}\nNumber of Slots: {}\nDate: {}\n\n".format(
+            ec.get("location").replace("\n", ""),
+            ec.get("cost"),
+            ec.get("num_slots"),
+            ec.get("date"))
+        body += text
+    print("About to send email...")
+    print("Email body: ", body)
+    gmail_user = os.environ.get('EMAIL_USER')
+    gmail_pwd = os.environ.get('EMAIL_PASSWORD')
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo
+    smtpserver.login(gmail_user, gmail_pwd)
+    mailing_list = [e for e in os.environ.get('MAILING_LIST').split(",") if e]
+    for to in mailing_list:
+        tag = datetime.datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
+        if not email_content:
+            tag = "--No slots found--"
+        header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:CoWIN Vaccination Slots {}\n'\
+            .format(tag)
+        print(header)
+        msg = header + '\n {} \n\n'.format(body)
+        smtpserver.sendmail(gmail_user, to, msg)
+        print("Email sent to " + to)
+    smtpserver.close()
 
 
 if __name__ == "__main__":
     while True:
-        processed_data = parse_data(data)
-        # processed_data = parse_data(request())
+        processed_data = parse_data(request())
         send_email(processed_data)
         time.sleep(SECONDS_TO_SLEEP)
-
-
-
-
-
-
-
